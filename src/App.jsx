ae59a32480b715b46126469fb03c6c9e7009fd81
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-10-30 06:16:14 +0530

    api call done

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='w-full h-screen flex flex-col background'>
-      <h1 className='bg-white rounded-sm'>RANDOM GIFS</h1>
-      <div className='flex flex-col'>
+    <div className='relative w-screen h-screen flex flex-col items-center background -mx-[355px] -my-[35px]'>
+      <h1 className='bg-white rounded-lg text-4xl font-bold w-11/12 text-center mt-[40px]'>RANDOM GIFS</h1>
+      <div className='flex flex-col w-full items-center gap-y-10 mt-[30px]'>
         <Random/>
         <Tag/>
       </div>
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+
+const API_KEY = import.meta.env.REACT_APP_GIPHY_API_KEY;
 
 const Random = () => {
-    return(
-        <div>
 
+    const [gif, setGif] = useState('')
+    
+    async function fetchData () {
+        const url=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+        const output = await axios.get(url);
+        console.log(output);
+    }
+
+    function clickHandler () {
+
+    }
+
+    return(
+        <div className="w-1/2 h-[500px] bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+            <h1 className="text-2xl underline uppercase font-bold">A Random Gif</h1>
+            <img src={gif} width="450" />
+            <button 
+            onClick={clickHandler}
+            className="bg-[#fdf4ff] w-10/12 text-lg py-1 rounded-lg "
+            >Generate</button>
         </div>
     )
 }
