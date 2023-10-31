d997ff5cbfe60196d9552ceb2ff716826cd9593c
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-11-01 00:56:54 +0530

    added spinner

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,19 +1,23 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Spinner from "./Spinner";
 
 const API_KEY = import.meta.env.VITE_APP_GIPHY_API_KEY;
 console.log(API_KEY)
 
 const Random = () => {
 
-    const [gif, setGif] = useState('')
+    const [gif, setGif] = useState('');
+    const [loading, setLoading] = useState(false);
     
     async function fetchData () {
+        setLoading(true)
         const url=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
         const {data} = await axios.get(url);
         const imageSource = data.data.images.downsized_large.url;
         console.log(data);
         setGif(imageSource);
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -27,7 +31,12 @@ const Random = () => {
     return(
         <div className="w-1/2 bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
             <h1 className="mt-[15px] text-2xl underline uppercase font-bold">A Random Gif</h1>
-            <img src={gif} width="450" />
+
+            {
+                loading ? (<Spinner/>) : (<img src={gif} width="450" />)
+            }
+
+            
             <button 
             onClick={clickHandler}
             className="bg-[#fdf4ff] mb-[20px] w-10/12 text-lg py-1 rounded-lg "
