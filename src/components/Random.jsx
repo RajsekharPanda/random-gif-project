f4e4e387fc7881af82002950aaa2a48c2064399e
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-10-31 23:55:30 +0530

    clickHandler done

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -11,7 +11,7 @@ const Random = () => {
     async function fetchData () {
         const url=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
         const {data} = await axios.get(url);
-        const imageSource = data.data.images.downsized_medium.url;
+        const imageSource = data.data.images.downsized_large.url;
         console.log(data);
         setGif(imageSource);
     }
@@ -25,12 +25,12 @@ const Random = () => {
     }   
 
     return(
-        <div className="w-1/2 h-[500px] bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
-            <h1 className="text-2xl underline uppercase font-bold">A Random Gif</h1>
+        <div className="w-1/2 bg-green-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
+            <h1 className="mt-[15px] text-2xl underline uppercase font-bold">A Random Gif</h1>
             <img src={gif} width="450" />
             <button 
             onClick={clickHandler}
-            className="bg-[#fdf4ff] w-10/12 text-lg py-1 rounded-lg "
+            className="bg-[#fdf4ff] mb-[20px] w-10/12 text-lg py-1 rounded-lg "
             >Generate</button>
         </div>
     )
