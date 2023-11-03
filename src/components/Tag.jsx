248854779098c20b53d47066e48b62623246a932
RajsekharPanda <rajsekharpandawork@gmail.com>
2023-11-03 19:31:26 +0530

    project jsx done

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -12,7 +12,7 @@ const Tag= () => {
     
     async function fetchData () {
         setLoading(true);
-        const url=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+        const url=`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
         const {data} = await axios.get(url);
         const imageSource = data.data.images.downsized_large.url;
         console.log(data);
@@ -33,7 +33,7 @@ const Tag= () => {
     }
     return(
         <div className="w-1/2 bg-blue-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]">
-            <h1 className="mt-[15px] text-2xl underline uppercase font-bold">Random Gif</h1>
+            <h1 className="mt-[15px] text-2xl underline uppercase font-bold">Random {tag} Gif</h1>
 
             {
                 loading ? (<Spinner/>) : (<img src={gif} width="450" />)
@@ -44,6 +44,7 @@ const Tag= () => {
             className="w-10/12 text-lg py-1 rounded-lg text-center"
             type="text"
             onChange={changeHandler}
+            value={tag}
             />
 
 
